f347bd10fcfa030ad4c8435e1ce896f979c349fa
Cream <79789097+m0t0rhektor@users.noreply.github.com>
2024-02-26 08:32:58 +0100

    Added product model

---
+++ src/models/product.ts
@@ -0,0 +1,19 @@
+import { Decimal128 } from 'mongodb';
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface Product extends Document {
+  name: string;
+  description: string;
+  price: Decimal128;
+  category: string; //TODO #1:
+  //More fields to be added here later
+}
+
+const ProductSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, default: 'This product has no description' },
+  price: { type: String, required: true },
+  category: { type: String, required: true }
+});
+
+const Product = mongoose.model<Product>('Product', ProductSchema);
