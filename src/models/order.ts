99824838cde76f2ea93dfd7f190a5fd1582f6bdb
Cream <79789097+m0t0rhektor@users.noreply.github.com>
2024-02-29 23:01:59 +0100

    Changed product.ts

--- src/models/order.ts
+++ src/models/order.ts
@@ -1,15 +1,18 @@
 import { Decimal128 } from 'mongodb';
 import mongoose, { Schema, Document, model, SchemaTypes } from 'mongoose';
 import { Product } from './product';
+import { user } from './user';
 
 export interface Order extends Document {
   products: Product[];
   total: Decimal128;
+  user: user;
   //More fields to be added here later
 }
 
 const OrderSchema: Schema = new Schema({
   description: { type: SchemaTypes.Array, required: true },
+  userId: SchemaTypes.ObjectId,
   total: {
     type: Schema.Types.Decimal128,
     required: true,
--- src/models/product.ts
+++ src/models/product.ts
@@ -1,5 +1,5 @@
 import { Decimal128 } from 'mongodb';
-import mongoose, { Schema, Document, model } from 'mongoose';
+import mongoose, { Schema, Document, model, SchemaTypes } from 'mongoose';
 
 export interface Product extends Document {
   name: string;
@@ -13,14 +13,14 @@ const ProductSchema: Schema = new Schema({
   name: { type: String, required: true },
   description: { type: String, default: 'This product has no description' },
   price: {
-    type: Schema.Types.Decimal128,
+    type: SchemaTypes.Decimal128,
     required: true,
     validate: {
       validator: (v: Decimal128) => parseFloat(v.toString()) >= 0,
       message: (props: any) => `${props.value} is a negative number`
     }
   },
-  category: { type: Schema.Types.ObjectId, required: true, ref: 'Category' }
+  category: { type: SchemaTypes.ObjectId, required: true, ref: 'Category' }
   //More fields to be added here later
 });
 const Product = model<Product>('Product', ProductSchema);
