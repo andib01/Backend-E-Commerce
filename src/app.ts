008e7ab5f0886c75421e3c8199d0e92a9a81e8c4
h679173 <679173@stud.hvl.no>
2024-03-03 13:55:37 +0100

    initial-testing

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import express, { Express } from 'express';
 import { startDB } from './db.js';
-import productRoutes from './routes/productRoutes';
+import productRoutes from './routes/productRoutes.js';
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 const port = process.env.SERVER_PORT || 8080;
@@ -18,6 +19,9 @@ function start() {
 
 function startHttpServer(app: Express) {
   app.use('/api', productRoutes);
+  app.get('/', (req, res) => {
+   res.send('Testing send');
+  }); 
   app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
   });
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,4 +1,4 @@
-import e from 'express';
+
 import Express from 'express';
 const router = Express.Router();
 
