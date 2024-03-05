7e4083edcc1dfd60b0e941218097980f1ff6f2be
Cream <79789097+m0t0rhektor@users.noreply.github.com>
2024-03-05 10:13:55 +0100

    Changed productTests.ts

--- src/tests/productTests.ts
+++ src/tests/productTests.ts
@@ -6,19 +6,10 @@ import { addCategory } from '../controllers/categoryController.js';
 
 async function addTest() {
   try {
-    // Create a new category
     const category = new Category({ name: 'TestCategory' });
-
-    // Save the category to the database
     await addCategory(category);
-
-    // Retrieve the _id of the newly created category
     const categoryId = category._id;
-
-    // Create a new product with the category _id
     const product = new Product({ name: 'TestProduct', price: Decimal128.fromString('123'), category: categoryId });
-
-    // Add the product to the database
     await addProduct(product);
 
     console.log('Product added successfully!');
